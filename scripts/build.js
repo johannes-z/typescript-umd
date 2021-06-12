5b5f2b7c5dbaddbc6c12d9b517c4517dffb5d209
johannes-z <johannes.zwirchmayr@gmail.com>
2021-06-12 23:30:38 +0200

    .

--- scripts/build.js
+++ scripts/build.js
@@ -2,8 +2,25 @@ const { build } = require('esbuild')
 
 build({
   entryPoints: ['./src/index.js'],
-  outfile: './tmp/build.js',
+  outfile: './public/build.js',
   minify: true,
   bundle: true,
-  format: 'esm'
+  target: 'es5',
+  format: 'iife',
+  globalName: '__export__',
+  banner: {
+    js: `
+;(function (root, factory) {
+  if (typeof define === 'function' && define.amd) {
+      // AMD. Register as an anonymous module.
+      define([], factory);
+  } else {
+      // Browser globals
+      root.typescript = factory();
+  }
+}(typeof self !== 'undefined' ? self : this, function () {`
+  },
+  footer: {
+    js: `return __export__; }));`
+  },
 }).catch(() => process.exit(1))
